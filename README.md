0f398c84fcbb97e38f23af8b0a5acb43b96a03cf
Akhmad Musa Hadi <freezyoff@gmail.com>
2018-11-12 23:12:52 +0700

    Merubah aturan struktur folder untuk support sub-domain
    Struktur Folder App/Http/Controllers: subdomain -> controllers file
    Struktur Folder App/DBModels: subdomain -> model file
    Struktur Folder Resources/Views: subdomain -> view file
    Struktur Folder Resources/Lang: lang code -> subdomain -> lang file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # jiwanala
+
+## Struktur App/Http/Controllers
+- subdomain -> page controllers
+
+## Struktur Folder App/DBModels
+- subdomain -> model file
+
+## Struktur Folder Resources/Views
+View:
+- subdomain -> page view
+
+## Struktur Folder Resources/Lang
+- lang code -> subdomain -> lang file
--- system/config/database.php
+++ system/config/database.php
@@ -62,6 +62,21 @@ return [
             'engine' => null,
         ],
 		
+		'jn_bimbel' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_BIMBEL', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+		
 		/*
 		'jn_accounting' => [
             'driver' => 'mysql',
