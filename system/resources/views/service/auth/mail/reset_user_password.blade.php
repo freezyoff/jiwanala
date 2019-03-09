aac6ba61f938dc0615be7dad2dbc567b3b2c8b08
Akhmad Musa Hadi <freezyoff@gmail.com>
2019-03-10 00:08:11 +0700

    test email

--- system/resources/views/service/auth/mail/reset_user_password.blade.php
+++ system/resources/views/service/auth/mail/reset_user_password.blade.php
@@ -3,8 +3,20 @@
 
 Kami menerima permintaan Atur Ulang Sandi. Klik tombol dibawah ini untuk mengarahkan kehalaman atur ulang sandi.
 
-@component('mail::button', ['url' => $token])
-Reset Password
+@component('mail::panel')
+<table>
+	<tbody>
+		<tr>
+			<td>Alamat URL</td>
+			<td>:</td>
+			<td>
+				<a href="{{$token}}" style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#3869d4" 
+					target="_blank" 
+					data-saferedirecturl="https://www.google.com/url?q={{$token}}&amp;source=gmail&amp;&amp;usg=AOvVaw2hTpkH2yDWsTzAgYq3xRWM">{{$token}}</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
 @endcomponent
 
 Sistem akan meminta password baru. Ganti password sesuai keinginan dan pastikan mengganti password secara berkala.
